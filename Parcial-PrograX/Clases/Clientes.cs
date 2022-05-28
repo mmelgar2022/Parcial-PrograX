e2805e90dd80c1fbb68fa3fdbccfd4060c9a4a9d
MynorVásquez <mmynnorev@gmail.com>
2022-05-28 09:57:34 -0600

    Creando Clases

---
+++ Parcial-PrograX/Clases/Clientes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial_PrograX.Clases
+{
+    public class Clientes
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string direccion { get; set; }
+        public string dpi { get; set; }
+        public  int idrubros { get; set; }
+
+
+    }
+}
